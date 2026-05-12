2d9dbd8ef10cbf1b7ba8f7a695d14ebed286da0b
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-05-12 17:06:32 -0300

    Atualizando DB
    Atualizando o caminho do DB

--- src/controller/auth-controller-register.ts
+++ src/controller/auth-controller-register.ts
@@ -3,8 +3,8 @@ import { ContentType } from '../utills/content-types'
 
 export const usersController = async (request: IncomingMessage, response: ServerResponse) => {
     response.writeHead(200, {'content-type' : ContentType.jsonUTF8})
-    response.end({
+    response.end(JSON.stringify({
         email: 'henriquebrancodasilvadias@gmail.com',
         password: 'Seila@123.mudar',
-    })
+    }))
 }
